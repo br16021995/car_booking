65c9183b0915fc7a62852f9e6255404721b45064
PriyankaVarma <priyankavarma0898@gmail.com>
2025-08-22 21:45:22 +0530

    changes

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = "eventcalendar.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ford_cars',
-        'USER': 'root',
-        'PASSWORD': 'Swaroopa@123',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '3306',
-        
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ford_cars',
+#         'USER': 'root',
+#         'PASSWORD': 'Swaroopa@123',
+#         'HOST': '127.0.0.1',
+#         'DATABASE_PORT': '3306',
+        
+#     }
+# }
 
 """
 ##CONECTAR CON POSTGRES
